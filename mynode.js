59a53b14d6590a8077e508110de7b3225d541768
bBravee <101563516+bBravee@users.noreply.github.com>
2024-06-17 11:43:00 +0200

    env files refactor

--- mynode.js
+++ mynode.js
@@ -5,10 +5,15 @@ const checkSign = "\u{2705}";
 const dotenv = require("dotenv").config({ path: "src/.env" });
 
 const envFile = `export const environment = {
-    API_KEY: '${process.env.API_KEY}',
-    STORAGE_BUCKET: '${process.env.STORAGE_BUCKET}',
-    MESSAGING_SENDER_ID: '${process.env.MESSAGING_SENDER_ID}',
-    APP_ID: '${process.env.APP_ID}',
+firebase: {
+    apiKey: '${process.env.API_KEY}',
+    authDomain: '${process.env.AUTH_DOMAIN}',
+    databaseURL: '${process.env.DB_URL}',
+    projectId: '${process.env.PROJECT_ID}',
+    storageBucket: '${process.env.STORAGE_BUCKET}',
+    messagingSenderId: '${process.env.MESSAGING_SENDER_ID}',
+    appId: '${process.env.APP_ID}',
+}
 };
 `;
 const targetPath = path.join(
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,13 +1,11 @@
 export const environment = {
   firebase: {
-    API_KEY: '',
-    authDomain: 'angular-training-app-60da2.firebaseapp.com',
-    databaseURL:
-      'https://angular-training-app-60da2-default-rtdb.firebaseio.com',
-    projectId: 'angular-training-app-60da2',
-    STORAGE_BUCKET: '',
-    MESSAGING_SENDER_ID: '',
-    APP_ID: '',
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagindSenderId: '',
+    appId: '',
   },
-  production: false,
 };
